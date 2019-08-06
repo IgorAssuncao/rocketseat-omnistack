ca9fca1107337c3a17c05280fdc38a85765e7075
IgorAssuncao <igor.ormonde@gmail.com>
2019-08-06 18:16:02 -0300

    Initialized repo

---
+++ backend/src/routes.js
@@ -0,0 +1,15 @@
+const express = require("express");
+
+const routes = express.Router();
+
+routes.get("/", (request, response) => {
+  return response.json({ message: `Hello, ${request.query.name}` });
+});
+
+routes.post("/devs", (request, response) => {
+  console.log(request.body);
+
+  return response.json({ ok: true });
+});
+
+module.exports = routes;
